83b59bcf5eb07247ca465d4ee9c080d57cacff4c
jw-zima <joanna.winter91@gmail.com>
2022-07-28 19:01:35 +0200

    extending profiling notebook

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -52,7 +52,7 @@ def draw_line_plot_for_each_k(df, x_var, y_var):
             + scale_x_continuous(breaks=df[x_var]))
 
 
-def plot_boxplot_by_cluster(df, num_var, cluster_var):
+def plot_boxplot_by_cluster(df, num_var, cluster_var, outliers_to_remove=None):
     """Make a box plot for each cluster using ggplot grammar of graphics
 
     Args:
@@ -66,11 +66,22 @@ def plot_boxplot_by_cluster(df, num_var, cluster_var):
     Examples:
     >>> plot_boxplot_by_cluster(dataset, "age", "cluster_kmeans")
     """
-    return print(ggplot(df)
-                 + geom_boxplot(aes(x=cluster_var, y=num_var,
-                                    fill=cluster_var))
-                 + ggtitle(num_var)
-                 + theme_light())
+    # print(num_var)
+    if not(pd.isnull(outliers_to_remove)):
+        if (type(outliers_to_remove) == float):
+            quantiles = df[num_var].quantile([outliers_to_remove,
+                                              1 - outliers_to_remove])
+            df = df.loc[((df[num_var] > quantiles.iloc[0])
+                        & (df[num_var] < quantiles.iloc[1])), :]
+    try:
+        g = (ggplot(df)
+             + geom_boxplot(aes(x=cluster_var, y=num_var, fill=cluster_var))
+             + ggtitle(num_var)
+             + theme_light()
+             + scale_fill_brewer(type="qual", palette="Dark2"))
+        return print(g)
+    except Exception:
+        pass
 
 
 def plot_scatter_by_cluster(df, num_var1, num_var2, cluster_var):
@@ -93,4 +104,37 @@ def plot_scatter_by_cluster(df, num_var1, num_var2, cluster_var):
                  + geom_point(aes(x=num_var1, y=num_var2,
                                   color=cluster_var))
                  + ggtitle(num_var1 + " vs " + num_var2)
-                 + theme_light())
+                 + theme_light()
+                 + scale_fill_brewer(type="qual", palette="Dark2"))
+
+
+def plot_share_of_binary_vars_per_clusters(df, var, cluster_var):
+    """Make a col plot colored by clusters using ggplot grammar of graphics
+    to present share of given binery var in each cluster
+
+    Args:
+    df (data.frame): dataset
+    var (string): name of variable for x axis
+    cluster_var (string): name of variable with clustes
+
+    Yields:
+    plot: Column plot with share of both binaries per cluster
+
+    Examples:
+    >>> plot_share_of_binary_vars_per_clusters(dataset, "education_cleaned",
+                                               "cluster_kmeans")
+    """
+    df = df.loc[:, [var, cluster_var]]
+    df = df.groupby([var, cluster_var]).value_counts()
+    df = df.reset_index().rename(columns={0: 'n'})
+    df['n_cluster'] = df.groupby(['cluster_kmeans']).n.transform('sum')
+    df['share_in_cluster'] = round(100 * df['n'] / df['n_cluster'], 1)
+
+    g = (ggplot(df)
+         + geom_col(aes(x=var, y="share_in_cluster", fill=cluster_var))
+         + facet_wrap('~' + cluster_var)
+         + theme_light()
+         + theme(legend_position="top")
+         + theme(axis_text_x=element_text(rotation=90, hjust=1))
+         + scale_fill_brewer(type="qual", palette="Dark2"))
+    print(g)
